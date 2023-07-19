d80bb24da4996fcccb4851755274b76907d44f52
pranavbadhan8 <pranavbadhan@example.com>
2023-07-19 11:19:31 +0530

    submitting code-Wed Jul 19 2023 11:19:27 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
 import './../styles/App.css';
+import React,{ useState } from "react";
+
 
 const App = () => {
+  
+  let [input,setInput] = useState("");
+      
+    function change(event){
+      setInput(event.target.value);
+    }
+   
   return (
-    <div>
+    <div className='container'>
         {/* Do not remove the main div */}
+        <div className="parent-container">
+           <h1>Parent Component</h1>
+           <p>{input}</p>
+
+        <div className="child-container">
+          <h2>Child component</h2>
+          <input onChange={change}/>
+        </div>
+        </div>  
     </div>
   )
 }
