3d48d33e4ab8524c06645c960343beaec0305ce6
pranavbadhan8 <pranavbadhan@example.com>
2023-07-19 11:46:33 +0530

    submitting code-Wed Jul 19 2023 11:46:30 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,14 @@ import React,{ useState } from "react";
 
 const App = () => {
   
-  let [input,setInput] = useState("");
+  let [input,setInput] = useState("Selected Option :");
       
-    function change(event){
-      setInput(event.target.value);
+    function change1(){
+      setInput("Selected Option : Option 1");
+    }
+
+    function change2(){
+      setInput("Selected Option : Option 2")
     }
    
   return (
@@ -16,13 +20,18 @@ const App = () => {
         {/* Do not remove the main div */}
         <div className='parent'>
            <h1>Parent Component</h1>
-           <p>{input}</p>
 
-        <div className='child'>
-          <h2>Child component</h2>
-          <input onChange={change} type='text'/>
+        <div className='child1'>
+          <h2>Child component 1</h2>
+          <button onClick={change1}>Option 1</button>
         </div>
-        </div>  
+
+        <div className='child2'>
+          <h2>Child component 2</h2>
+          <button onClick={change2}>Option 2</button>
+        </div>
+           <p>{input}</p>  
+      </div>
     </div>
   )
 }
